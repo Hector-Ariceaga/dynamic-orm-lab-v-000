86d6430017b028ce631c449d405ca802e4bd277a
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-06-29 16:26:20 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -38,7 +38,7 @@ class InteractiveRecord
   def values_for_insert
     values = []
     self.class.column_names.each do |column_name|
-      values << 
+      values << "'#{column_name"
   end 
   
 end
