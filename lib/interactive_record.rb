beeb5f58a824b4e019a411d4cf7cce5b73e1207f
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-06-29 16:47:57 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -61,7 +61,7 @@ class InteractiveRecord
   end 
   
   def self.find_by(attribute)
-    binding.pry
+    
     sql = <<-SQL
       SELECT * FROM #{self.table_name} WHERE #{attribute.keys[0]} = ?
     SQL
