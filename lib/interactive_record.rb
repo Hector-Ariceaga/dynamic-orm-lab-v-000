b21eb9b0c69347fb807b9126771321363b09d77d
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-06-29 16:39:38 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -49,8 +49,7 @@ class InteractiveRecord
     SQL
     
     DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}").flatten.first
-    binding.pry
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
   
 end
