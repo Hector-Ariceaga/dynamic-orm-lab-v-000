103456172f4c2b6cf66b3d609dd7e392fc035e29
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-06-29 15:45:59 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -13,6 +13,6 @@ class InteractiveRecord
     table_info = DB[:conn].execute(sql)
     column_names = []
     table_info.each do |column|
-      column_names << 
+      column_names << column[name]
     
 end
